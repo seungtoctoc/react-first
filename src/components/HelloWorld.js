e4fa4eaea6041817a8c0d993f2043fd0f8a6de20
seungtoctoc <dltmxor31@naver.com>
2024-01-23 13:52:57 +0900

    feat: create name div

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function HelloWorld() {
+    return (
+        <div>
+            <h1>
+                Hello, world!
+            </h1>
+            <p>
+                This is my first react application.
+            </p>
+        </div>
+    )
+}
