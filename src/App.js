3ce9886307d7d6057656d3bb4d581aaf27b5f477
seungtoctoc <dltmxor31@naver.com>
2024-01-23 13:53:39 +0900

    feat: import components page

--- src/App.js
+++ src/App.js
@@ -6,15 +6,9 @@ import HelloWord from './components/HelloWorld';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h4>
-          Hi, I'm seungtaek
-        </h4>
-        <h6>
-          G'day, mate
-        </h6>
-      </header>
+      <HelloWord>
+        
+      </HelloWord>
     </div>
   );
 }
