b8b8d61ad69340eb8b5774016dd88b01fd5aeb8d
seungtoctoc <dltmxor31@naver.com>
2024-01-23 14:54:51 +0900

    fix: modify display of blink button

--- src/App.js
+++ src/App.js
@@ -8,14 +8,11 @@ import Blink from './components/Blink';
 function App() {
   return (
     <div className="App">
-
-      <Blink text="hahahahaha">
-      </Blink>
-
       <CaptionImage imgUrl="https://mblogthumb-phinf.pstatic.net/MjAyMTA0MTRfMzEg/MDAxNjE4MzkzNzYyMTI3.2O_46TmKTYGbXMeu-W_9wdfAAw-tYLPzzRO2ZhB7hesg.iBmAqi17fck3tC4907gGaFIp1IalksYgIGCdjhhSuzAg.JPEG.eu2/1618393754909.jpg?type=w800" caption="ahahahahaha">
       </CaptionImage>
 
-
+      <Blink text="ahahahaha">
+      </Blink>
     </div>
   );
 }
--- src/components/Blink.js
+++ src/components/Blink.js
@@ -3,16 +3,24 @@ import React, {useEffect, useState} from 'react';
 export default function Blink({text}) {
     const [showText, setShowText] = useState(true);
 
-    useEffect(() => {
-        const timeoutId = setInterval(() => {
-            setShowText(showText => !showText);
-        }, 1000)
-        return () => {clearInterval(timeoutId)}
-    }, [])
+    // useEffect(() => {
+    //     const timeoutId = setInterval(() => {
+    //         setShowText(showText => !showText);
+    //     }, 1000)
+
+    //     return () => {clearInterval(timeoutId)}
+    // }, [])
 
     return (
-        <div>
+        <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+            <button style={{margin:'5px', width:'100px'}} onClick={() => {
+                setShowText(!showText);
+            }}>
+                click
+            </button>
+            
             {showText? text : null}
+        
         </div>
     )
 }
